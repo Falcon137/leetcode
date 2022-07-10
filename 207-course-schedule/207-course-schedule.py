c8551df64479f437267bd79be194c0e1700b7bdb
ac13711 <46797996+ac13711@users.noreply.github.com>
2022-07-10 13:56:22 -0400

    Time: 199 ms (20.19%), Space: 16.8 MB (54.30%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,31 +1,25 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         adjL = {i:[] for i in range(numCourses)}
-        dependencies = [0 for _ in range(numCourses)]
-        print(adjL)
-        print(dependencies)
-        for p in prerequisites:
-            a,b = p[0],p[1]
+        for a,b in prerequisites:
             adjL[b].append(a)
-            dependencies[a] += 1
-        
-        available = []
-        for idx,d in enumerate(dependencies):
-            if d == 0:
-                available.append(idx)
-            
-        while available:
-            k = available.pop()
-            targets = adjL[k]
-            for t in targets:
-                dependencies[t] -= 1
-                if dependencies[t] == 0:
-                    available.append(t)
-                    
-        for d in dependencies:
-            if d != 0:
-                return False
+        visited = {}
+        trap = {}
+        for i in range(numCourses):
+            if i not in visited:
+                if self.dfs(i,visited,adjL,trap):
+                    return False
         return True
+    
+    def dfs(self,i,visited,adjL,trap):
+        if i in visited:
+            trap[i] = True
+            return False
+        visited[i] = True
+        for neighbor in adjL[i]:
+            if self.dfs(neighbor,visited,adjL,trap):
+                return True
+        if i in trap:
+            return True
+        return False
             
-        
-        
