6c64e38d2c696c9bc58ab87ac24a7654660bf849
ac13711 <46797996+ac13711@users.noreply.github.com>
2022-07-16 17:04:56 -0400

    Time: 167 ms (42.41%), Space: 16.8 MB (54.52%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,26 +1,26 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        path = {}
-        visited = {}
+        path = {i:False for i in range(numCourses)}
+        visited = {i:False for i in range(numCourses)}
         adjL = [[] for _ in range(numCourses)]
         for a,b in prerequisites:
             adjL[b].append(a)
         for i in range(numCourses):
-            if i not in visited:
-                if self.dfs(i,path,adjL,visited)==False:
+            if visited[i] == False:
+                if self.isCyclic(i,path,adjL,visited):
                     return False
         return True
     
-    def dfs(self,node_idx,path,adjL,visited):
-        if node_idx in path:
-            return False
-        if node_idx in visited:
+    def isCyclic(self,node_idx,path,adjL,visited):
+        if path[node_idx]:
             return True
+        if visited[node_idx]:
+            return False
         path[node_idx] = True
         visited[node_idx] = True
         for neighbor in adjL[node_idx]:
-            if self.dfs(neighbor,path,adjL,visited) == False:
-                return False
-        del path[node_idx]
-        return True
+            if self.isCyclic(neighbor,path,adjL,visited):
+                return True
+        path[node_idx]=False
+        return False
         
